83ec06cd81e62f769f9bf4ce6655b47d2e0d63fa
Anonymous <ParsWebDesigners@gmail.com>
2022-02-08 10:50:08 +0330

    work with observable & subscribe, & create message service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component } from '@angular/core';
+import { MessageService } from './message.service';
 import { USER } from './user-interface';
 import { UserService } from './user.service';
 
@@ -8,17 +10,26 @@ import { UserService } from './user.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private userService:UserService){}
+  constructor(private userService:UserService,public messageService:MessageService){}
   ngOnInit(): void {
     this.getUsers();
   }
   title = 'user-manager';
   users:USER[]=[];
-  selectedUser?:USER;
+  selectedUser?:USER;  
   getUsers():void{
-    this.users=this.userService.getUsers();
+    this.userService.getUsers().subscribe(usersData=>this.users=usersData);
+    this.displayConsole();
+  }
+  displayConsole():void{
+    for(let message of this.messageService.messages){
+      console.log('%c'+message,'color:#5bc0de');
+    }
   }
   select(user:USER){
     this.selectedUser=user;
+    console.clear();
+    this.messageService.add(`the user with name '${user.name}'\n& age ${user.age}\n& id ${user.id}\nIS SELECTED!`)
+    this.displayConsole();
   }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import { USER } from './user-interface';
 import { users } from './mock-users';
+import { Observable,of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  constructor() { }
-  getUsers():USER[]{
-    return users;
+  constructor(private messageService:MessageService) { }
+  getUsers():Observable<USER[]>{
+    const usersData=of(users);
+    this.messageService.add('usersData Fetched!');
+    return usersData;
   }
 }
